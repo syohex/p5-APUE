4a71ae763b5f98cf85cd56f64d9f6e01b63960aa
Syohei YOSHIDA <syohex@gmail.com>
2011-04-06 00:12:15 +0900

    重複していた部分を削除しました.   - set group idのために 07777と &を行うように修正   - 不要な print文の修正

--- section4/4_4.pl
+++ section4/4_4.pl
@@ -7,13 +7,8 @@ use Fcntl ':mode';
 # chmod関数の使用例
 my $file = shift or die "Usage: $0 file\n";
 
-my $orig = (stat $file)[2] & 0777;
+my $orig = (stat $file)[2] & 07777;
 
 # turn on set-group-ID and turn off group-execute
-my $new_permission = (($orig & ~S_IXGRP) | S_ISGID);
-printf "test = %o\n", $new_permission;
-chmod (($orig & ~S_IXGRP) | S_ISGID), $file or die "Can't chmod $file\n";
-
-my $mode = (stat $file)[2] & 0x777;
-
-printf "original mode: 0%o, after mode: 0%o\n", $orig, $mode;
+my $new_permission = (($orig & ~S_IXGRP) | S_ISGID) & 07777;
+chmod $new_permission, $file or die "Can't chmod $file\n";
