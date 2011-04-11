97d358bae399dc6611e8d3fc4645dfd5c9013d3e
Syohei YOSHIDA <syohex@gmail.com>
2011-04-11 19:59:41 +0900

    8_3を追加しました  - プロセスの終了状態を出力する

---
+++ section8/8_3.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use POSIX;
+
+# プロセスの終了状態を出力する
+
+sub pr_exit {
+    my $status = shift;
+
+    if (POSIX::WIFEXITED($status)) {
+        printf "normal termination, exit status = %d\n"
+        , POSIX::WEXITSTATUS($status);
+    } elsif (POSIX::WIFSIGNALED($status)) {
+        printf "bnormal termination, signal number = %d\n"
+        , POSIX::WTERMSIG($status);
+    } elsif (POSIX::WIFSTOPPED($status)) {
+        printf "child stopped, signal number = %d\n"
+        , POSIX::WSTOPSIG($status)
+    }
+}
