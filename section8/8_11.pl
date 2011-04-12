2405c0d3f241ffc10a6a47cce58e4035c7a1b57b
Syohei YOSHIDA <syohex@gmail.com>
2011-04-12 20:31:16 +0900

    8_11を追加しました  - 解釈実行ファイルに入れたPythonプログラム

---
+++ section8/8_11.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# 解釈実行ファイルに入れたPythonプログラム
+
+my $length = scalar @ARGV;
+for my $i ( 0..($length-1) ) {
+    printf "ARG[%d] = %s\n", $i, $ARGV[$i];
+}
