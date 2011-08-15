7c1196446476e47411d8a0dcd6e2fa9c51331bca
Syohei YOSHIDA <syohex@gmail.com>
2011-08-15 19:37:12 +0900

    Set file handle with GetTerminalSize();

--- section11/11_12.pl
+++ section11/11_12.pl
@@ -2,14 +2,14 @@
 use strict;
 use warnings;
 
-use POSIX;
+use POSIX ();
 use Term::ReadKey qw(GetTerminalSize);
 
 # ウィンドウサイズを表示する
 
 sub pr_winsize {
-    my $fd = shift;
-    my ($wchar, $hchar, $wpixels, $hpixels) = GetTerminalSize();
+    my $fh = shift;
+    my ($wchar, $hchar, $wpixels, $hpixels) = GetTerminalSize($fh);
 
     printf "%d rows, %d columns\n", $hchar, $wchar;
 }
@@ -18,13 +18,14 @@ unless (POSIX::isatty(*STDIN)) {
     die "STDIN is not a tty\n";
 }
 
-pr_winsize(POSIX::STDIN_FILENO);
+pr_winsize(*STDIN);
 
 local $SIG{WINCH} = sub {
     print "SIGWINCH received\n";
-    pr_winsize(POSIX::STDIN_FILENO);
+    pr_winsize(*STDIN);
 };
 
+print "My pid = $$\n";
 while (1) {
     POSIX::pause();
 }
